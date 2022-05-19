b0c8c41f54f1dd7ae31911ea9d96b90189b65942
Gary Antier <gary.antier@rafi.ph>
2022-05-19 11:21:32 +0800

    feat: command bar
    ~added command bar component.
    ~updated navigation view.
    ~bug fixes.

---
+++ src/fluent-blazor-components/Components/FluentAppBarButton.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using sagemode_fluent_blazor_components.Utilities.Classes;
+
+namespace sagemode_fluent_blazor_components.Components
+{
+    public partial class FluentAppBarButton : FluentComponentBase
+    {
+        [Parameter] public string? Icon { get; set; }
+
+        [Parameter] public string? Label { get; set; }
+
+        [Parameter] public string? Modifier { get; set; }
+
+        [Parameter] public string? Key { get; set; }
+    }
+}
---
+++ src/fluent-blazor-components/Components/FluentCommandBar.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using sagemode_fluent_blazor_components.Utilities.Classes;
+
+namespace sagemode_fluent_blazor_components.Components
+{
+    public partial class FluentCommandBar : FluentComponentBase
+    {
+        [Parameter] public bool? IsOpen { get; set; }
+    }
+}
