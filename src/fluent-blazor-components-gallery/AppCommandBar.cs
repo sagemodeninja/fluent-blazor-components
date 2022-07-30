7f1efdea6e9051aea5ef792d299fa676051a1d2e
sagemodeninja <contact@garyantier.com>
2022-07-30 16:33:25 +0800

    feat: auto command bar

---
+++ src/fluent-blazor-components-gallery/AppCommandBar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace fluentblazorcomponentsgallery
+{
+    public class AppCommandBar
+    {
+        public string Command { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Label { get; set; }
+
+        public string? Modifier { get; set; }
+
+        public string Key { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+}
+
---
+++ src/fluent-blazor-components/Components/FluentCommandBarAuto.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using sagemode_fluent_blazor_components.Utilities.Classes;
+using sagemode_fluent_blazor_components.Utilities.Enums;
+
+namespace sagemode_fluent_blazor_components.Components
+{
+    /// <summary>
+    /// A variant of FluentCommandBar with templating.
+    /// </summary>
+    public partial class FluentCommandBarAuto<TItem> : FluentComponentBase
+    {
+        [Parameter] public RenderFragment<TItem>? ItemTemplate { get; set; }
+
+        [Parameter] public IEnumerable<TItem>? Items { get; set; }
+
+        [Parameter] public CommandBarAppearance? DefaultLabelPosition { get; set; }
+
+        [Parameter] public bool? IsOpen { get; set; }
+    }
+}
+
